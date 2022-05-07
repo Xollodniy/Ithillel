1a163096cf66bfda6014e50b4881ebde1040e625
Andriy Pytel <104265472+Xollodniy@users.noreply.github.com>
2022-05-07 22:43:12 +0300

    Add files via upload

--- Dz2.php
+++ Dz2.php
@@ -5,12 +5,10 @@ $b = 40;
 echo '$a = ' . $a . PHP_EOL;
 echo '$b = ' . $b . PHP_EOL;
 
-    if ($a === 10) {
-        $a = 40;
-    }
-    if ($b === 40) {
-        $b = 10;
-    }
+$c = $a;
+$a = $b;
+$b = $c;
+
 
 echo '$a = ' . $a . PHP_EOL;
 echo '$b = ' . $b . PHP_EOL;

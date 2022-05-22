5316fccba9dd79157fa2870af66c361a53fc38e0
Andriy Pytel <feel.the.taste13@gmail.com>
2022-05-22 17:18:41 +0300

    Homeworks

---
+++ Dz17.php
@@ -0,0 +1,11 @@
+<?php
+$arr = [10, -20, -30, 40, 50, -60, -70, 80, -75];
+    foreach ($arr as $key => $value){
+        if($arr[$key] < 0) {
+            $arr[$key] = -1 * $arr[$key];
+        }
+        }
+print_r($arr);
+
+
+

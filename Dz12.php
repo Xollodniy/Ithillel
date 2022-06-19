3989d2684486de5aff07db7e0d9126663a6c3b7e
Andriy Pytel <feel.the.taste13@gmail.com>
2022-06-19 14:26:56 +0300

    Update Dz12.php

--- Dz12.php
+++ Dz12.php
@@ -1,9 +1,14 @@
 <?php
-$input = '123212';
-$reverse = strrev($input);
-
-    if ($input == $reverse) {
-        echo 'Паліндром';
-    } else {
-        echo 'НЕ Паліндром';
-    }
+$str = 'радар';
+  function strrev_enc($str): string{
+    $str = iconv('utf-8', 'windows-1251', $str);
+    $str = strrev($str);
+    $str = iconv('windows-1251', 'utf-8', $str);
+    return $str;
+}
+$result = strrev_enc($str);
+        if($str === $result){
+            echo 'Palindrome';
+        } else{
+            echo 'NOT Palindrome';
+        }
